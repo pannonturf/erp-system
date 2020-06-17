d02104e4d7ff072aeba79bbde68b744f795cbd7c
pannonturf <67041208+pannonturf@users.noreply.github.com>
2020-06-17 08:44:38 +0200

    cleaning

--- pc2.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-setcookie("pc","login2",time()+(60*60*24*365*10));
-echo '<div class="alert alert-success center-block" role="alert">Cookie set (2)</div>';
-
-?>
--- temp/pc.php
+++
@@ -1,5 +0,0 @@
-<?php
-setcookie("pc","login2",time()+(60*60*24*365*10));
-?>
-
-<h3>Cookie set</h3>
